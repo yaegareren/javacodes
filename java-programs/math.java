6b8eb5178b6f5e1fe0516fc7b4e61b8e02d08fdd
Shree Mangalam <sshreemangalam@gmail.com>
2024-02-21 16:07:27 +0530

    added math

---
+++ java-programs/math.java
@@ -0,0 +1,12 @@
+public class math {
+  public static void main(String[] args) {
+    double x = 3.45, y = 6.8; 
+
+    double max_Num = Math.max(x, y); 
+
+    double square_Root = Math.sqrt(30);
+
+    System.out.println(max_Num);
+    System.out.println(square_Root);
+  }
+}
