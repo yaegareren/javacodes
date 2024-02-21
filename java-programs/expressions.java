8d5c0521820da626b26b64ff136a34a0fb76c9fa
Shree Mangalam <sshreemangalam@gmail.com>
2024-02-21 15:50:57 +0530

    added expressions

---
+++ java-programs/expressions.java
@@ -0,0 +1,12 @@
+public class expressions {
+  public static void main(String[] args) {
+    double friends = 10;
+    
+    friends = friends + 2; 
+    friends++; 
+    friends = (double) friends / 3; 
+    System.out.println(friends);
+
+    // typecasting
+  }
+}
